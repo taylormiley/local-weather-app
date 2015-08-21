de9ab609548ca2bec2b392d90b3365b35905ab12
BrendonPierson <brendonpierson@gmail.com>
2015-08-21 11:14:44 -0500

    added google client id and secret

---
+++ javascripts/googleAuth.js
@@ -0,0 +1,10 @@
+define(function(require){
+  var fb = require("firebase");
+
+  // client id
+  // 71024305615-053gk4kit5a69hjs32hmvimvd65c4rf9.apps.googleusercontent.com
+
+  // secret
+  // fpjTUaQHSp3nEGVjvxJyCNv_
+
+});
