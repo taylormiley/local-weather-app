f0097150a97bf397d2bed0f14c12e038a4dd3753
Robin Burton <1.robinburton@gmail.com>
2015-08-21 13:33:14 -0500

    zip-call changes

--- javascripts/zip-call.js
+++ javascripts/zip-call.js
@@ -1,15 +1,22 @@
-define(function(){
-  return {
-   queryZip: function(zipCall) { 
+define(function(require){
+  var $ = require("jquery"),
+      Q = require("q");
+  return function(zipCall) { 
       
       $.ajax({
-            url: "http://api.zippopotam.us/us/" + zipCall
+            url: "http://api.zippopotam.us/us/" + zipCall,
+            method: "GET"
           }).done(function(data) {
-            console.log(data);
+            deferred.resolve(data);
+            console.log("data", data);
             return data.places["place name"];
-            
-      });
-          
-     }
+          }).fail(function(xhr, status, error) {
+            deferred.reject(error);
+          });
+          return deferred.promise;
   };
 });
+
+
+
+
