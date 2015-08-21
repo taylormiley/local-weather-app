2ab02a782c30fd3891f6ec3f94831905fc19a949
Robin Burton <1.robinburton@gmail.com>
2015-08-21 14:09:23 -0500

    weather-call

--- javascripts/eventHandlers.js
+++ javascripts/eventHandlers.js
@@ -2,7 +2,9 @@ define(function(require){
   var $ = require("jquery"),
       userZip = require("userZip"),
       zipCall = require("zip-call"),
-      currentWeather = require("weather-call");
+      currentWeather = require("weather-call"),
+      templates = require("get-templates");
+
   
   
 
@@ -11,7 +13,10 @@ define(function(require){
     var zip = zipCall(userZip());
     zip.then(function(data){
       console.log("data", data);
-      currentWeather(data);
+      currentWeather(data).then(function(data){
+        console.log("currentWeatherdata", data);
+        $("#currentWeather").html(templates.template(data));
+      });
     }).fail(function(data){
       console.log("data", data);
     }).done();
